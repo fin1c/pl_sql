af34ed560cf44ca704c0b6b0ffddbbe42c25a55e
fin1c <91886479+fin1c@users.noreply.github.com>
2025-01-15 21:25:21 +0200

    Update and rename proba.sql to logs.sql

---
+++ Tables/logs.sql
@@ -0,0 +1,7 @@
+CREATE TABLE logs
+        ( id NUMBER,
+        appl_proc VARCHAR2(50),
+        message VARCHAR2(2000),
+        log_date DATE DEFAULT SYSDATE,
+        CONSTRAINT id_pk PRIMARY KEY(id)
+        );
