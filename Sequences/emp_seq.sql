5ae20ae41b03a70372af309e97ebffacee9be7ac
fin1c <91886479+fin1c@users.noreply.github.com>
2025-02-01 19:38:46 +0200

    Create emp_seq.sql

---
+++ Sequences/emp_seq.sql
@@ -0,0 +1,14 @@
+DECLARE
+    v_max_dep_id NUMBER;
+BEGIN
+    
+    SELECT NVL(MAX(em.employee_id), 0) 
+    INTO v_max_dep_id 
+    FROM andriyi_9wd.employees em;
+
+    EXECUTE IMMEDIATE 'CREATE SEQUENCE emp_seq
+                       MINVALUE 1
+                       START WITH ' || (v_max_dep_id + 1) || ' INCREMENT BY 1 
+                       CACHE 20';
+END;
+/
