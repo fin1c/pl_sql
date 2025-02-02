704e324e5cf7b6bb3c716e446206d18b2ca2f1ca
fin1c <91886479+fin1c@users.noreply.github.com>
2025-02-02 22:01:40 +0200

    Update 110.sql

--- Scripts/110.sql
+++ Scripts/110.sql
@@ -1,1 +1,22 @@
+SELECT *
+FROM andriyi_9wd.employees;
 
+
+SELECT *
+FROM andriyi_9wd.employees_history;
+
+SELECT *
+FROM andriyi_9wd.logs l
+order by l.log_date desc;
+
+ DELETE FROM andriyi_9wd.employees em
+                        WHERE em.employee_id = 207;
+
+
+
+BEGIN
+  util.fire_an_employee(p_employee_id => 208);
+
+ --    commit;  
+END;
+/
