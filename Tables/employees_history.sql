edb5e30575a363313bcb836d16a33d1e09b81c69
fin1c <91886479+fin1c@users.noreply.github.com>
2025-02-02 21:56:46 +0200

    Update employees_history.sql

--- Tables/employees_history.sql
+++ Tables/employees_history.sql
@@ -1,1 +1,10 @@
+CREATE TABLE andriyi_9wd.employees_history
+             (employee_id NUMBER,
+              first_name VARCHAR2(20),
+              last_name VARCHAR2(25),
+              hire_date DATE,
+              job_id VARCHAR2(10),
+              salary NUMBER(8,2),
+              department_id NUMBER(4),
+              dismissal_date DATE);
 
