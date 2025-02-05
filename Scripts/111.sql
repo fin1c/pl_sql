605ae1dae381bddcbf676fd84d4080461b8c1dc4
fin1c <91886479+fin1c@users.noreply.github.com>
2025-02-05 20:59:13 +0200

    Update 111.sql

--- Scripts/111.sql
+++ Scripts/111.sql
@@ -1,1 +1,18 @@
+SELECT *
+FROM andriyi_9wd.employees;
+
+
+
+
+
+
+
+
+BEGIN
+  util.change_attribute_employee(p_employee_id => 231,
+                                 p_first_name => 'Andrii232');
+
+ --    commit;  
+END;
+/
 
