9b02e9a349c7214c68973cab4d18668787b84499
fin1c <91886479+fin1c@users.noreply.github.com>
2025-03-01 19:02:43 +0200

    Update 113.sql

--- Scripts/113.sql
+++ Scripts/113.sql
@@ -30,19 +30,7 @@ SELECT *
 FROM cur_exchange;
 
 
-BEGIN
-    sys.dbms_scheduler.create_job(job_name => 'update_curr',
-                                  job_type => 'PLSQL_BLOCK',
-                                  job_action => 'BEGIN andriyi_9wd.util.api_nbu_sync(); end;',
-                                  start_date => SYSDATE,
-                                  repeat_interval => 'FREQ=DAILY; BYHOUR=6; BYMINUTE=00',
-                                  end_date => TO_DATE(NULL),
-                                  job_class => 'DEFAULT_JOB_CLASS',
-                                  enabled => TRUE,
-                                  auto_drop => FALSE,
-                                  comments => 'Оновлення курсу валют');
-END;
-/ 
+
 
 --Вимкнути шедулер
 BEGIN
